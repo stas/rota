29d1f5bf7578e2d84d87ff742df801af4aff040f
Stas SUȘCOV <stas@net.utcluj.ro>
2011-06-01 00:24:30 +0300

    Added csv export format

---
+++ includes/templates/csv.php
@@ -0,0 +1,33 @@
+<?php
+// Serve as CSV mimetype
+header("Content-Type: text/csv");
+header("Content-Disposition: inline; filename=rota_" . time() . ".csv ");
+
+// Build the CSV content
+if( $days && $intervals && $locations ) {
+    foreach ( $locations as $l ) {
+        echo $l['title'];
+        
+        foreach ( $days as $d )
+            echo "," . $d['title'];
+        
+        echo "\n";
+        
+        foreach ( $intervals as $i ) {
+            for ( $j = 0; $j < $l['size']; $j++ ) {
+                echo $i['title'];
+                foreach ( $days as $d ) {
+                    $uids = array_values( $users[ $d['name'] ][ $i['name'] ][ $l['name'] ] );
+                    if ( isset( $uids[$j] ) )
+                        echo "," . get_the_author_meta( 'display_name', $uids[$j] );
+                    else
+                        echo ",n/a";
+                }
+                echo "\n";
+            }
+            echo "\n";
+        }
+        echo "\n";
+    }
+}
+?>
